c52fa4bc2dbfad3415c74c5d80b5c6f6d286231e
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2022-01-20 17:11:35 +0100

    init concept, simple but it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import useLocalStorage from 'use-local-storage';
+
+
 
 function App() {
+
+  const [numbersArray, setNumbersArray] = useState([1, 2, 3]);
+  const [number, setNumber] = useState(0);
+
+  //initial useEffect that runs on first render, checks to see if localData contains the key,
+  //if it exists it sets the state with the parsed data from local storage
+  React.useEffect(() => {
+    const localData = localStorage.getItem('numbersArray');
+    if (localData) {
+      setNumbersArray(JSON.parse(localData));
+    }
+  }, []);
+
+  //if we want to add elements to local storage, this is where it happens.
+  //this runs on every render  
+  React.useEffect(() => {
+    console.log('updating storage')
+    localStorage.setItem('numbersArray', JSON.stringify(numbersArray));
+  }, [numbersArray]);
+
+  //this pushes new values to loalStorage
+  const pushToArray = (number) => {
+    console.log('hello')
+    setNumbersArray([...numbersArray, Number(number)])
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={number} onChange={((e) => setNumber(e.target.value))}></input>
+      <button onClick={() => pushToArray(number)}>push</button>
     </div>
   );
-}
+
+};
 
 export default App;
